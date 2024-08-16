851bf9abb8133db7b16c1a12782c2417ea9dc030
amelia8872 <jkramelia8872@gmail.com>
2024-08-16 12:55:30 -0400

    adding the result page and stripe payment

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -7,6 +7,22 @@ const formatAmountForStripe = (amount) => {
   return Math.round(amount * 100);
 };
 
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams;
+  const session_id = searchParams.get('session_id');
+
+  try {
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
+    return NextResponse.json(checkoutSession, { status: 200 });
+  } catch (error) {
+    console.error('Error retrieving checkout session', error);
+    return NextResponse.json(
+      { error: 'An unexpected error occurred' },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(req) {
   const params = {
     mode: 'subscription',
@@ -31,6 +47,15 @@ export async function POST(req) {
       'origin'
     )}/result?session_id={CHECKOUT_SESSION_ID}`,
   };
-  const checkoutSession = await stripe.checkout.sessions.create(params);
-  return NextResponse.json(checkoutSession, { status: 200 });
+
+  try {
+    const checkoutSession = await stripe.checkout.sessions.create(params);
+    return NextResponse.json(checkoutSession, { status: 200 });
+  } catch (error) {
+    console.error('Error creating checkout session', error);
+    return NextResponse.json(
+      { error: 'An unexpected error occurred' },
+      { status: 500 }
+    );
+  }
 }
--- app/result/page.js
+++ app/result/page.js
@@ -1,12 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
 import getStripe from '@/utils/get-stripe';
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const ResultPage = () => {
-  const router = useRouter();
+  const rotuer = useRouter();
   const searchParams = useSearchParams();
   const session_id = searchParams.get('session_id');
 
@@ -15,60 +16,88 @@ const ResultPage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchCheckoutSession() {
-      if (!session_id) {
-        return;
-      }
+    const fetchCheckcoutSession = async () => {
+      if (!session_id) return;
 
       try {
         const res = await fetch(
-          '/api/checkout_session?session_id=${session_id}'
+          `/api/checkout_session?session_id=${session_id}`
         );
         const sessionData = await res.json();
-
         if (res.ok) {
           setSession(sessionData);
         } else {
           setError(sessionData.error);
         }
-      } catch (err) {
-        setError('An unexpected error occurred.');
+      } catch (e) {
+        console.error('An error occured');
       } finally {
         setLoading(false);
       }
-    }
+    };
 
-    fetchCheckoutSession();
+    fetchCheckcoutSession();
   }, [session_id]);
 
   if (loading) {
-    return <Container maxWidth = "100vw" sx={{textAlign="center", mt:4}}>
-      <CircularProgress />
-      <Typography variant="h6">Loading...</Typography>
-
-    </Container>>
+    return (
+      <Container
+        maxWidth="100vw"
+        sx={{
+          textAlign: 'center',
+          mt: 4,
+        }}
+      >
+        <CircularProgress />
+        <Typography variant="h6">Loading...</Typography>
+      </Container>
+    );
   }
 
   if (error) {
-    return <Container maxWidth = "100vw" sx={{textAlign="center", mt:4}}>
-      <CircularProgress />
-      <Typography variant="h6">{error}</Typography>
-
-    </Container>>
+    return (
+      <Container
+        maxWidth="100vw"
+        sx={{
+          textAlign: 'center',
+          mt: 4,
+        }}
+      >
+        <CircularProgress />
+        <Typography variant="h6">{error}</Typography>
+      </Container>
+    );
   }
 
   return (
-   <Container maxWidth = "100vw" sx={{textAlign="center", mt:4}}>
-      {session.payment._status === 'paid' ? (<><Typography variant='h4'>Thank you for purchasing.</Typography>
-      <Box sx={{mt:22}}><Typography>Session ID: {session_id}</Typography>
-      <Typography variant={body1}>We have received your payment. You will receive an email with the order details shortly.</Typography>
-      </Box></>):(
-        <><Typography variant='h4'>Payment Failed.</Typography>
-
-        <Typography variant={body1}>Your payment was not seccessful!</Typography>
-        </Box></>
+    <Container
+      maxWidth="100vw"
+      sx={{
+        textAlign: 'center',
+        mt: 4,
+      }}
+    >
+      {session.payment_status === 'paid' ? (
+        <>
+          <Typography variant="h4">Thank you for purchasing</Typography>
+          <Box sx={{ mt: 22 }}>
+            <Typography variant="h6">Session Id: {session_id}</Typography>
+            <Typography variant="body1">
+              We have recieved your payment. You will recieve an email with the
+              order details shortly.
+            </Typography>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Typography variant="h4">Payment failed</Typography>
+          <Box sx={{ mt: 22 }}>
+            <Typography variant="body1">Please try again.</Typography>
+          </Box>
+        </>
       )}
-
-    </Container>>
+    </Container>
   );
 };
+
+export default ResultPage;
