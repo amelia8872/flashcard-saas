aa904e0b58b3d639a5a1b1e42bdad5267d67bd5d
amelia8872 <jkramelia8872@gmail.com>
2024-08-15 22:50:02 -0400

    flash card rendered

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -2,12 +2,12 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 
 const systemPrompt = `
-You are a flashcard creator. Your task is to generate flashcards for various subjects. Each flashcard should have a question on one side and the answer on the other side. Ensure that the questions are clear and concise, and the answers are accurate and informative. The flashcards should be suitable for learners of all levels, from beginners to advanced. Please provide a variety of questions, including definitions, explanations, and examples where applicable.
+You are a flashcard creator. Your task is to generate flashcards for various subjects. Each flashcard should have a question on one side and the answer on the other side. Ensure that the questions are clear and concise, and the answers are accurate and informative. The flashcards should be suitable for learners of all levels, from beginners to advanced. Please provide a variety of questions, including definitions, explanations, and examples where applicable. Only generate 10 flashcards per request.
 Return in the following JSON format:
 {
   "flashcards": [{
-  "front":str,
-  "back":str
+  "front":"Front of the flashcard",
+  "back":"Back of the flashcard"
   }]
 }
 `;
@@ -17,7 +17,7 @@ export async function POST(req) {
 
   const data = await req.text();
 
-  const completion = await openai.chat.completion.create({
+  const completion = await openai.chat.completions.create({
     messages: [
       {
         role: 'system',
@@ -29,10 +29,11 @@ export async function POST(req) {
       },
     ],
     model: 'gpt-4o',
-    response_format: { type: 'json_object' }, // Corrected this line
+    response_format: { type: 'json_object' },
   });
+  // console.log(completion.choices[0].message.content);
 
   const flashcards = JSON.parse(completion.choices[0].message.content);
 
-  return NextResponse.json(flashcards.flashcard);
+  return NextResponse.json(flashcards.flashcards);
 }
